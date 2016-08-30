5c18652500efab24ea58d3a4cd34f573794968bf
kushal <kushaldeveloper@gmail.com>
2016-08-30 15:00:37 -0400

    use old db connect

--- includes/or-dbinfo.php
+++ includes/or-dbinfo.php
@@ -19,29 +19,31 @@ $dbuser = "openroom1";
 $dbpass = "MslkgTAjiPzJXVa3YX58phcWnM66gbW6MoyZNIZDo6eqaIUgMwb9wlfKXcVZYW5";
 $dbdatabase = "openroom1";
 
-/** @var mysqli $connection */
-$connection = mysql_connect($dbhost, $dbuser, $dbpass, $dbdatabase);
+mysql_connect($dbhost, $dbuser, $dbpass) or die('Can\'t connect to the database. Error: ' . mysql_error());
+mysql_select_db($dbdatabase) or die('Can\'t connect to the database. Error: ' . mysql_error());
 
 $lmresult = mysql_query("SELECT * FROM settings WHERE 1;");
-while ($lmrecord = mysql_fetch_array($lmresult)) {
+while($lmrecord = mysql_fetch_array($lmresult)){
 	$settings[$lmrecord["settingname"]] = $lmrecord["settingvalue"];
 }
 
-foreach ($_POST as $key => $value) {
-	if (!is_array($value)) {
+foreach($_POST as $key=>$value){
+	if(!is_array($value)){
 		$_POST[$key] = mysql_real_escape_string($value);
-	} else {
-		foreach ($value as $key2 => $value2) {
+	}
+	else{
+		foreach($value as $key2=>$value2){
 			$_POST[$key][$key2] = mysql_real_escape_string($value2);
 		}
 	}
 }
 
-foreach ($_GET as $key => $value) {
-	if (!is_array($value)) {
+foreach($_GET as $key=>$value){
+	if(!is_array($value)){
 		$_GET[$key] = mysql_real_escape_string($value);
-	} else {
-		foreach ($value as $key2 => $value2) {
+	}
+	else{
+		foreach($value as $key2=>$value2){
 			$_GET[$key][$key2] = mysql_real_escape_string($value2);
 		}
 	}
